b4bc450a9a156f944a1a1fa330369b93103fb181
LINYUHAN1288 <1057000273@qq.com>
2020-02-03 20:00:44 +0800

    yuhan

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <home-header></home-header>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    components: {
+        HomeHeader
+    },
+    data () {
+        return {
+            
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    },
+    activated() {
+
+    }
+
+}
+</script>
